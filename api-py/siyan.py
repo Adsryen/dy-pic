5661d399c93c76825833a65bf6421c12d13c259b
adsryen <prl1594959462@gmail.com>
2021-05-09 05:19:14 +0800

    fix

--- api-py/siyan.py
+++ api-py/siyan.py
@@ -8,7 +8,7 @@ Created on Wed Jan 13 17:20:54 2021
 import os
 
 #文件的绝对路径
-file_path = "E:\GitHub Desktop\GitHub\dy-pic\huihui\\"
+file_path = "E:\GitHub Desktop\GitHub\dy-pic\siyan\\"
 #CDN前缀
 web_path = "https://cdn.jsdelivr.net/gh/"
 #GITHUB账户名
@@ -16,7 +16,7 @@ user_path = "anderson-ryen"
 #仓库名
 warehouse_path = "dy-pic"
 #仓库内文件夹
-img_path = "huihui"
+img_path = "siyan"
 #合并path
 api_path = os.path.join( web_path + user_path + "/" + warehouse_path + "/" + img_path + "/")
 
@@ -25,13 +25,13 @@ print("")
 print("前缀预览: " + api_path)
 print("")
 print("=" *50)
-print("huihui已完成")
-print("地址预览：E:\GitHub Desktop\GitHub\dy-pic\\api-py\huihui.txt")
+print("siyan已完成")
+print("地址预览：E:\GitHub Desktop\GitHub\dy-pic\\api-py\siyan.txt")
 print("=" *50)
 print("")
 
 if __name__ == '__main__':
     filelist = os.listdir(file_path)
-    with open(r"E:\GitHub Desktop\GitHub\dy-pic\api-py\huihui.txt",'w',encoding='utf-8') as f:
+    with open(r"E:\GitHub Desktop\GitHub\dy-pic\api-py\siyan.txt",'w',encoding='utf-8') as f:
         for file in filelist:
             f.write(api_path+file+'\n')
